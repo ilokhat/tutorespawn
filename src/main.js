5834638682faa59e0d57e21ae9dae95e93fb2fbb
ilokhat <lartane@gmail.com>
2018-07-20 15:27:05 +0200

    deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import * as itowns from 'itowns';
-import DARK from './layers/DARK';
-import bati from './layers/bati';
-
-let positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 25000000 };
-let viewerDiv = document.getElementById('viewerDiv');
-let globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe);
-globeView.addLayer(DARK);
-globeView.addLayer(bati);
